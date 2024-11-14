e1ffa5b7b74c9ae8d0952e258b82c41d86914336
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-11-14 20:05:56 +0000

    Template ready

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,46 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
-// Class:       CS150-xx (Replace xx with your section number)
-// Assignment:  Hello World
-// Purpose:     Display the message Hello World to the screen
-// Hours:       0.25
+// Author:      
+// Date:        
+// Class:       
+// Assignment:  
+// Purpose:     
+// Hours:       
 //******************************************************************************
 
 #include <iostream>
+#include <fstream>
+#include <cstring>
 
 using namespace std;
 
 int main () {
+  string fileName;
+  ifstream inFile;
+  ofstream outFile;
+  int numChars = 0;
+  char ch;
 
-  cout << "Hello World" << endl;
-  
+  // TODO #1: Open a file for reading gettysburg.txt. Prompt the user
+  //          for the filename and error check that the file was opened
+  //          correctly using is_open. Use a while loop.
+
+
+  // TODO #2: Output the number of characters in the file. The correct answer
+  //          is 1461
+
+
+  // TODO #3: Ask the user to enter a file for writing to. Open the file
+  //          for writing
+
+
+  // TODO #4: Create a file gettysburg_formatted.txt that replaces the
+  //          first blank after character 70 with a line return.
+  inFile.clear ();
+  inFile.seekg (0);
+
+  inFile.close ();
+  outFile.close ();
+
   return EXIT_SUCCESS;
 }
